0aad00a45905378e0fe809fdb85a562e5ad09a9d
Annette Dm <51707930+annettedm@users.noreply.github.com>
2025-03-24 15:58:30 +0300

    add score fnlity, rounds

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-
-    // Plan or pseudocode your solution.
-    // Write the code.
-// Test your code to make sure it works.
-console.log("inside");
-   
-// rock paper scissors
+  
+let userScore = 0;
+let compScore = 0;
 
 function makeUserChoice() {
   const userChoice = prompt("Select rock, paper or scissors").toLowerCase();
@@ -21,32 +17,81 @@ function makeComputerChoice() {
     case 3: return "scissors";
   }
 }
-// 3. results are compared, the winner or ties are denoted
-function compareResults() {
-  const userChoice = makeUserChoice();
-  const compChoice = makeComputerChoice();
 
-  console.log(`user choice is ${userChoice}`);
-  console.log(`computer choice is ${compChoice}`);
-
-  console.log(decideWinner(userChoice, compChoice));
-  // compare results
-  // tell the winner or tie
+function capitalize(word) {
+  const firstLetter = word[0].toUpperCase(); 
+  word = firstLetter + word.slice(1);
+  return word;
 }
 
 function decideWinner(userChoice, compChoice) {
   if ((userChoice == "rock" && compChoice == "rock")
     || (userChoice == "paper" && compChoice == "paper")
     || (userChoice == "scissors" && compChoice == "scissors")) {
-    return "It's a tie";
+    console.log("It's a tie.");
+    return "tie";
   } else if ((userChoice == "rock" && compChoice == "scissors")
     || (userChoice == "scissors" && compChoice == "paper")
     || userChoice == "paper" && compChoice == "rock") {
-    return "User wins";
+    console.log(`User wins. ${capitalize(userChoice)} ${userChoice == "scissors" ? "beat" : "beats"} ${compChoice}.`);
+    return "user";
   } else {
-    return "Computer wins";
+    console.log(`Computer wins. ${capitalize(compChoice)} ${compChoice == "scissors" ? "beat" : "beats"} ${userChoice}.`);
+    return "computer";
   }
 }
 
+function updateScores(roundResult) {
+  switch (roundResult) {
+    case "tie":
+      userScore++;
+      compScore++;
+      break;
+    case "user":
+      userScore++;
+      break;
+    case "computer":
+      compScore++;
+      break;
+  }
+}
+
+function printScores() {
+  console.log("----")
+  console.log(`User score: ${userScore}.`);
+  console.log(`Computer score: ${compScore}.`);
+}
+// 3. results are compared, the winner or ties are denoted
+function playRound() {
+  const userChoice = makeUserChoice();
+  const compChoice = makeComputerChoice();
+
+  console.log(`User choice is ${userChoice}.`);
+  console.log(`Computer choice is ${compChoice}.`);
+
+  const winner = decideWinner(userChoice, compChoice);
+
+  updateScores(winner);
+  printScores();
+  // compare results
+  // tell the winner or tie
+}
+
+
+function playGame() {
+  console.log("Let's start a game.")
+
+  for (let i = 1; i <= 5; i++) {
+    console.log('------');
+    console.log(`Round ${i}`);
+    playRound();
+    console.log('------');
+  }
+
+  console.log("The game is over.");
+}
+
+// play entire game with 5 rounds 
+
 // function that starts the game 
-compareResults();
+playGame();
